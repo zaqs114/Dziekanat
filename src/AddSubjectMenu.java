a79bf50c3764983d8a8632d9528a7761f8928e68
zaqs114 <zaqs@10g.pl>
2017-04-14 13:25:04 +0200

    next gui parts

--- src/AddSubjectMenu.java
+++ src/AddSubjectMenu.java
@@ -1,14 +1,33 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class AddSubjectMenu extends JDialog {
     private JPanel contentPane;
     private JButton buttonOK;
     private JButton buttonCancel;
+    private JTextField nazwaPrzedmiotuTextField;
+    private JTextField oznaczeniePrzedmiotuTextField;
 
     public AddSubjectMenu() {
         setContentPane(contentPane);
         setModal(true);
         getRootPane().setDefaultButton(buttonOK);
+        buttonCancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+        buttonOK.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Subjects subjects = new Subjects();
+                subjects.setSubjectName(nazwaPrzedmiotuTextField.getText());
+                subjects.setSubjectID(oznaczeniePrzedmiotuTextField.getText());
+
+            }
+        });
     }
 
     public static void main(String[] args) {
--- src/Subjects.java
+++ src/Subjects.java
@@ -9,7 +9,13 @@ public class Subjects {
     public void setSubjectName(String subjectName) {
         this.subjectName = subjectName;
     }
-
+    public String getSubjectID() {
+        return subjectID;
+    }
+    public void setSubjectID(String subjectID) {
+        this.subjectID = subjectID;
+    }
     private String subjectName;
+    private String subjectID;
 
 }
